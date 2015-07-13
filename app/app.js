6700de876c5ebddbeac26209a042688030559182
jacprada <jacopo.prada@gmail.com>
2015-07-13 13:02:42 +0100

    clear button implemented

--- app/app.js
+++ app/app.js
@@ -8,17 +8,13 @@ var Dom = {};
 //function to generate interactive grid, setup gameplay features and launch actual game 
 
 Dom.setUp = function() {
-	$("body").append("<div id='header'></div");
-	$("#header").append("<input type='text' id='display' readonly>");
-	$("#header").append("<input type='button' id='clear'>");
-	$("body").append("<div id='main'></div");
 
 	//allow player to select the size of the grid (number of squares) and determine the ideal number of lines
 	this.numberOfSquares = parseInt(prompt("Select the number of squares in the grid", "9"));
 	this.width = Math.round(Math.sqrt(this.numberOfSquares));
 
 	//generate customized grid: squares are "li" determined by user and contained within one "ul"
-	$("#main").append("<ul id='grid'></ul>");
+	$("body").append("<ul id='grid'></ul>");
 	this.grid = $("#grid");
 	this.grid.css("width", this.width * 100 + "px");
 	for (var i = 0; i < this.numberOfSquares; i++) {
@@ -42,6 +38,12 @@ Dom.setUp = function() {
 
 	//make the game run by selecting the first cell
 	this.cells.on("click", this.getInput);
+
+	this.display = $("#display");
+	this.clear = $("#clear");
+	this.clear.on("click", function(){
+		location.reload();
+	});
 };
 
 
@@ -52,31 +54,35 @@ Dom.getInput = function() {
 	if (Dom.counter % 2 === 0) {
 
 		//save cell selected by the player together with extra cell (+1 for horizontal move)
-		var playerXIndex = Dom.cells.index(this);
-		var playerXXIndex = playerXIndex + 1;
+		Dom.playerXIndex = Dom.cells.index(this);
+		Dom.playerXXIndex = Dom.playerXIndex + 1;
 
 		//make sure player selects cells on same line (horizontal move only)
-		if (Math.floor(playerXIndex/Dom.width) !== Math.floor(playerXXIndex/Dom.width)) {
+		if (Math.floor(Dom.playerXIndex/Dom.width) !== Math.floor(Dom.playerXXIndex/Dom.width)) {
+			Dom.display.val("You can't!");
 			console.log("Nope....");
 		} 
 
 		//determine if player can actually select the chosen cells (if they exist and are not already selected)
-		else if (Dom.canMove(playerXIndex) && Dom.canMove(playerXXIndex)) {
+		else if (Dom.canMove(Dom.playerXIndex) && Dom.canMove(Dom.playerXXIndex)) {
 
 			//mark selected cells, assign value null in the comparison array and increase value of counter 
 			$(this).css("background-color", "yellow");
-			$(Dom.cells[playerXXIndex]).css("background-color", "yellow");
-			Dom.fullGrid[playerXXIndex] = null;
-			Dom.fullGrid[playerXIndex] = null;
+			$(Dom.cells[Dom.playerXXIndex]).css("background-color", "yellow");
+			Dom.fullGrid[Dom.playerXXIndex] = null;
+			Dom.fullGrid[Dom.playerXIndex] = null;
 			Dom.counter += 1;
 			
 			//test winning conditions for the other player: if he is unable to select cells, game is over 
 			if (Dom.testWin(Dom.width)) {
+				Dom.display.val("X wins");
 				console.log("X Wins");
 			} else {
+				Dom.display.val("go on!");
 				console.log("Continue...");
 			}
 		} else {
+			Dom.display.val("you can't!");
 			console.log("Nope....");
 		}
 
@@ -99,11 +105,14 @@ Dom.getInput = function() {
 			
 			//test winning conditions for the other player: if he is unable to select cells, game is over
 			if (Dom.testWin(1)) {
+				Dom.display.val("O wins!");
 				console.log("O Wins");
 			} else {
+				Dom.display.val("go on!");
 				console.log("Continue");
 			}
 		} else {
+			Dom.display.val("you can't!");
 			console.log("Nope....");
 		}
 	}
@@ -130,10 +139,12 @@ Dom.testWin = function(value) {
 	var win = true;
 
 	//run the functions that determine if players are allowed to move on all elements of the comparison array 
-	for (i; i < len; i++) {
 		if (Dom.canMove(i) && Dom.canMove(i + value)) {
 			win = false;
-			break;
+			if (value == 1) {
+				if (Math.floor(Dom.playerXIndex/Dom.width) !== Math.floor(Dom.playerXXIndex/Dom.width)) {
+					win = true;
+			}
 		}
 	}
 	return win;
